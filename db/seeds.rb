7b6bbe63243e9459bd167c2b1f772933c5fe8194
MikeHenze <yashuachrist@gmail.com>
2018-08-29 14:17:13 -0400

    creating products in seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+products = Product.new(name: "wrist straps", price: 12, image_url: "https://images-na.ssl-images-amazon.com/images/I/51W3YqceAWL.jpg", description: "straps used for heavy weight lifting")
